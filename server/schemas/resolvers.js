59a129b3d3d72981473946aa20423d3416777104
Erin Keller <aestheticartist@gmail.com>
2023-06-29 12:34:13 -0400

    added saveBook mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const resolvers = {
     // get the user
     getUser: async (_, __, { userId }) => {
       // if no userid, throw an error
-      if (!userId) throw new Error('User not authenticated.');
+      if (!userId) throw new Error('Not authenticated.');
       // otherwise, find the user by ID and return
       return await User.findById(userId);
     },
@@ -20,6 +20,14 @@ const resolvers = {
       const { username, email, password } = input;
       // create the user with username, email, and password
       return await User.create({ username, email, password });
+    },
+    // save the book to the users savedBooks array
+    saveBook: async (_, { bookData }, { userId }) => {
+      // if no userid, throw an error
+      if (!userId) throw new Error('Not authenticated.');
+      // find user by id, and push the book to the savedBooks array
+      // new: true returns the updated object
+      return await User.findByIdAndUpdate(userId, { $push: { savedBooks: bookData } }, { new: true });
     },
   },
 };
