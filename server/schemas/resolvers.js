85422928b4dc4c64eb2522225d0e5cca8bc96217
Erin Keller <aestheticartist@gmail.com>
2023-06-29 11:56:27 -0400

    added create user mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,14 +2,26 @@
 const { Class } = require('../models');
 
 const resolvers = {
+  // retrieve data from the server
   Query: {
+    // get the user
     getUser: async (_, __, { userId }) => {
       // if no userid, throw an error
       if (!userId) throw new Error('User not authenticated.');
       // otherwise, find the user by ID and return
       return await User.findById(userId);
     },
-  }
+  },
+  // modify data on the server
+  Mutation: {
+    // create a new user on sign up
+    createUser: async (_, { input }) => {
+      // initialize variables
+      const { username, email, password } = input;
+      // create the user with username, email, and password
+      return await User.create({ username, email, password });
+    },
+  },
 };
 
 // export the resolvers
